8a23c7ed8f4aed22b6ec2c06b9b4a01a736ad043
Sponsor Magnet <92038043+unkn0wnnnnnn@users.noreply.github.com>
2022-01-10 04:40:02 -0500

    Update firewall.sh

--- sample/sample-config-files/firewall.sh
+++ sample/sample-config-files/firewall.sh
@@ -19,49 +19,38 @@ iptables -P OUTPUT DROP
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -F
+iptables -X
 
 # Set default policies
-iptables -P OUTPUT ACCEPT
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
+iptables -P OUTPUT REJECT
+iptables -P INPUT REJECT
+iptables -P FORWARD REJECT
 
-# Prevent external packets from using loopback addr
-iptables -A INPUT -i eth0 -s $LOOP -j DROP
-iptables -A FORWARD -i eth0 -s $LOOP -j DROP
-iptables -A INPUT -i eth0 -d $LOOP -j DROP
-iptables -A FORWARD -i eth0 -d $LOOP -j DROP
+# Prevent external packets from REJECT using loopback addr
+iptables -A INPUT -s $LOOP -j REJECT
+iptables -A INPUT -d $LOOP -j REJECT
 
 # Anything coming from the Internet should have a real Internet address
-iptables -A FORWARD -i eth0 -s 192.168.0.0/16 -j DROP
-iptables -A FORWARD -i eth0 -s 172.16.0.0/12 -j DROP
-iptables -A FORWARD -i eth0 -s 10.0.0.0/8 -j DROP
-iptables -A INPUT -i eth0 -s 192.168.0.0/16 -j DROP
-iptables -A INPUT -i eth0 -s 172.16.0.0/12 -j DROP
-iptables -A INPUT -i eth0 -s 10.0.0.0/8 -j DROP
+iptables -A INPUT -s 192.168.0.0/16 -j REJECT
+iptables -A INPUT -s 172.16.0.0/12 -j REJECT
+iptables -A INPUT -s 10.0.0.0/8 -j REJECT
 
 # Block outgoing NetBios (if you have windows machines running
 # on the private subnet).  This will not affect any NetBios
 # traffic that flows over the VPN tunnel, but it will stop
 # local windows machines from broadcasting themselves to
 # the internet.
-iptables -A FORWARD -p tcp --sport 137:139 -o eth0 -j DROP
-iptables -A FORWARD -p udp --sport 137:139 -o eth0 -j DROP
-iptables -A OUTPUT -p tcp --sport 137:139 -o eth0 -j DROP
-iptables -A OUTPUT -p udp --sport 137:139 -o eth0 -j DROP
+iptables -A INPUT -p tcp --sport 137:139 -j REJECT
+iptables -A INPUT -p udp --sport 137:139 -j REJECT
+iptables -A OUTPUT -p tcp --sport 137:139 -j REJECT
+iptables -A OUTPUT -p udp --sport 137:139 -j REJECT
 
-# Check source address validity on packets going out to internet
-iptables -A FORWARD ! -s $PRIVATE -i eth1 -j DROP
+# Check source address validity -j REJECT
+iptables -A FORWARD -s $PRIVATE -j REJECT
 
-# Allow local loopback
-iptables -A INPUT -s $LOOP -j ACCEPT
-iptables -A INPUT -d $LOOP -j ACCEPT
-
-# Allow incoming pings (can be disabled)
-iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
-
-# Allow services such as www and ssh (can be disabled)
-iptables -A INPUT -p tcp --dport http -j ACCEPT
-iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+#$#Allow local loopback
+#iptables -A INPUT -s $LOOP -j ACCEPT
+#iptables -A INPUT -d $LOOP -j ACCEP
 
 # Allow incoming OpenVPN packets
 # Duplicate the line below for each
@@ -79,30 +68,29 @@ iptables -A INPUT -p tcp --dport ssh -j ACCEPT
 
 iptables -A INPUT -p udp --dport 1194 -j ACCEPT
 
-# Allow packets from TUN/TAP devices.
+# DisAllow packets from TUN/TAP devices.
 # When OpenVPN is run in a secure mode,
-# it will authenticate packets prior
+# it should authenticate packets prior
 # to their arriving on a tun or tap
-# interface.  Therefore, it is not
+# interface.  
+# 
+# It is not.
+
+# Therefore,
 # necessary to add any filters here,
-# unless you want to restrict the
-# type of packets which can flow over
-# the tunnel.
+#  
+# you want to restrict the
+# type of packets which can flow over the tuns
 
-iptables -A INPUT -i tun+ -j ACCEPT
-iptables -A FORWARD -i tun+ -j ACCEPT
-iptables -A INPUT -i tap+ -j ACCEPT
-iptables -A FORWARD -i tap+ -j ACCEPT
-
-# Allow packets from private subnets
-iptables -A INPUT -i eth1 -j ACCEPT
-iptables -A FORWARD -i eth1 -j ACCEPT
+# DisAllow packets from private subnets
+iptables -A INPUT -j REJECT
+iptables -A FORWARD -j REJECT
 
 # Keep state of connections from local machine and private subnets
-iptables -A OUTPUT -m state --state NEW -o eth0 -j ACCEPT
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -m state --state NEW -o eth0 -j ACCEPT
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+# iptables -A OUTPUT -m state --state NEW -o eth0 -j ACCEPT
+# iptables -A INPUT -m state --state ESTABLISHED,RELATED -j REJECT
+# iptables -A FORWARD -m state --state NEW -o eth0 -j ACCEPT
+# iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # Masquerade local subnet
 iptables -t nat -A POSTROUTING -s $PRIVATE -o eth0 -j MASQUERADE
